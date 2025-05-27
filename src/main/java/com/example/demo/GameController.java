89f9922ee06276b4fb8ba0d0d9771a8563b58f97
bartoszOlewinski <bartosz.olewinski@gmail.com>
2025-05-27 15:05:01 +0200

    Added extra get endpoint for findgamebyid

--- src/main/java/com/example/demo/GameController.java
+++ src/main/java/com/example/demo/GameController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,7 +11,7 @@ import java.util.List;
 @RestController
 public class GameController {
 
-    private List<Game> db = List.of(new Game("1", "test", "testfile"));
+    private List<Game> db = List.of(new Game("0", "test", "testfile"));
 
     @GetMapping("/hello")
     public String hello() {
@@ -20,5 +21,10 @@ public class GameController {
     @GetMapping("/games")
     public List<Game> getLibrary() {
         return db;
+    }
+
+    @GetMapping("/games/{id}")
+    public Game getGameById(@PathVariable String id) {
+        return db.get(Integer.parseInt(id));
     }
 }
